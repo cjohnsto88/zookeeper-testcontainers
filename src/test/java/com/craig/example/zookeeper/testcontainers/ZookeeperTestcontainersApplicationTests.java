efacad5fe7e89f3724baae2add1c2ff7cc5edace
Craig Johnston <cjohnsto88@gmail.com>
2025-05-07 20:54:57 +0100

    Made test confirm updates to data in Zookeeper and also added github actions CI file

--- src/test/java/com/craig/example/zookeeper/testcontainers/ZookeeperTestcontainersApplicationTests.java
+++ src/test/java/com/craig/example/zookeeper/testcontainers/ZookeeperTestcontainersApplicationTests.java
@@ -1,5 +1,8 @@
 package com.craig.example.zookeeper.testcontainers;
 
+import org.apache.curator.framework.recipes.atomic.AtomicValue;
+import org.apache.curator.framework.recipes.atomic.DistributedAtomicInteger;
+import org.apache.curator.framework.recipes.shared.SharedCount;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +23,22 @@ class ZookeeperTestcontainersApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private SharedCount sharedCount;
+
+    @Autowired
+    private DistributedAtomicInteger distributedAtomicInteger;
+
     @Test
     void contextLoads() throws Exception {
         mockMvc.perform(post("/zookeeper").param("count", "100"))
                 .andExpect(status().isCreated());
+
+        assertThat(sharedCount.getCount()).isEqualTo(100);
+
+        AtomicValue<Integer> value = distributedAtomicInteger.get();
+        assertThat(value.succeeded()).isTrue();
+        assertThat(value.postValue()).isGreaterThan(0);
     }
 
 }
