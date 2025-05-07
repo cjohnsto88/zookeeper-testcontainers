444764b689ea650f53162e3440321b4bd6819045
Craig Johnston <cjohnsto88@gmail.com>
2025-05-07 21:11:46 +0100

    Waiting for port to be available to ensure Zookeeper up and added actuator endpoints

--- src/test/java/com/craig/example/zookeeper/testcontainers/TestcontainersConfiguration.java
+++ src/test/java/com/craig/example/zookeeper/testcontainers/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package com.craig.example.zookeeper.testcontainers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -7,6 +8,8 @@ import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.StandardEnvironment;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.Map;
@@ -21,7 +24,9 @@ class TestcontainersConfiguration {
                 .withEnv("ZOOKEEPER_CLIENT_PORT", "2181")
                 .withEnv("ZOOKEEPER_TICK_TIME", "2000")
                 .withEnv("ZOOKEEPER_INIT_LIMIT", "5")
-                .withEnv("ZOOKEEPER_SYNC_LIMIT", "2");
+                .withEnv("ZOOKEEPER_SYNC_LIMIT", "2")
+                .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("zookeeper")))
+                .waitingFor(Wait.forListeningPort());
 
         zookeeperContainer.start();
 
